45dbbb3e66025dd9ed69f0f19a2a00b29d91a7c1
Mi Soulimani <Soulimaniprosoulim@gmail.com>
2023-03-10 12:40:27 +0100

    add more user friendly error messages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,22 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
+
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message })
-    }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    
+    let customError   = {
+        statusCode : err.statusCode || StausCodes.INTERNAL_SERVER_ERROR,
+        errorMsg : err.msg || 'Something went wrong Please try later gain!'
+//     if (err instanceof CustomAPIError) {
+//         return res.status(err.statusCode).json({ msg: err.message })
+//     }
+        
+        if(err.code === 11000){
+            customError.errorMsg = 'Please provide a valid name, email or password'
+            custom.statusCode = 400
+        }
+//     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    return  res.status(customError.statusCode).json({msg: customError.errorMsg})
 
 }
 
